802d6c59747e80041696b0014aea1b36bd1e3e93
VictorFerreira999 <96120410+VictorFerreira999@users.noreply.github.com>
2024-08-30 12:35:51 -0300

    correção

--- condicionais/controle-fluxo/src/PlanoOperadora.java
+++ condicionais/controle-fluxo/src/PlanoOperadora.java
@@ -6,12 +6,15 @@ public class PlanoOperadora {
         switch (plano) {
             case "T": {
                 System.out.println("5Gb youtube");
+                break;
             }
             case "M": {
                 System.out.println("Whats e instagram grátis");
+                break;
             }
             case "B": {
                 System.out.println("100 minutos de ligação");
+                break;
 
             }
         }
