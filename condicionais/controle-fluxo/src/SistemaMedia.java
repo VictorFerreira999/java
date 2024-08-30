34947e60cd77aaaa1cf37866647b5c3f3ce13378
VictorFerreira999 <victorferreira9990@gmail.com>
2024-08-30 09:28:36 -0300

    SWITCH

---
+++ condicionais/controle-fluxo/src/SistemaMedia.java
@@ -0,0 +1,18 @@
+public class SistemaMedia {
+   public static void main(String[] args) {
+      String sigla = "M";
+      switch (sigla) {
+         case "P":
+            System.out.println("PEQUENO");
+            break;
+         case "M":
+            System.out.println("MEDIO");
+            break;
+         case "G":
+            System.out.println("GRANDE");
+            break;
+         default:
+            break;
+      }
+   }
+}
