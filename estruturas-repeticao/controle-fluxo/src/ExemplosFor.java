345be88e7b955e0f30ef0834e5062b455600b217
VictorFerreira999 <victorferreira9990@gmail.com>
2024-08-31 17:29:24 -0300

    FOR

---
+++ estruturas-repeticao/controle-fluxo/src/ExemplosFor.java
@@ -0,0 +1,8 @@
+public abstract class ExemplosFor {
+   public static void main(String[] args) {
+      for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos ++){
+         System.out.println("Contando carneirinhos" + carneirinhos);
+      }
+      System.out.println("Dormindo z z z z z z");
+   }
+}
