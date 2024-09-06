d93f66404321769f8419bd3739d5595f566c4c9b
VictorFerreira999 <victorferreira9990@gmail.com>
2024-09-06 09:17:39 -0300

    construtor

--- POO/BASIC/ESCOLA/src/Escola.java
+++ POO/BASIC/ESCOLA/src/Escola.java
@@ -1,8 +1,8 @@
 public class Escola {
    public static void main(String[] args) {
-      Aluno felipe = new Aluno();
-      felipe.setNome("Felipe");
-      felipe.setIdade(8);
+      Aluno felipe = new Aluno("Felipe", 8);
+      //felipe.setNome("Felipe");
+      //felipe.setIdade(8);
 
       System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos");
 
