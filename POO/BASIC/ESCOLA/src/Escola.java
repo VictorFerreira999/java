eb5834362abd06fdce1c77c55ddaa06d3cac9e91
VictorFerreira999 <victorferreira9990@gmail.com>
2024-09-06 08:33:57 -0300

    get e set

--- POO/BASIC/ESCOLA/src/Escola.java
+++ POO/BASIC/ESCOLA/src/Escola.java
@@ -1,6 +1,11 @@
 public class Escola {
    public static void main(String[] args) {
       Aluno felipe = new Aluno();
-      felipe.nome
+      felipe.setNome("Felipe");
+      felipe.setIdade(8);
+
+      System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos");
+
+
    }
 }
